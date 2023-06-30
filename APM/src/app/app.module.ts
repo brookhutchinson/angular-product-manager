d2e2977e62a4c6bb0f7c133729f10be343f23842
Brook Hutchinson <Brookhutchinson@outlook.com>
2023-06-30 09:40:35 -0500

    Setup Angular Router

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -2,10 +2,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 // feature modules
 import { HomeModule } from './home/home.module';
 import { ProductModule } from './product/product.module';
+
+// app routing module
+import { AppRoutingModule } from './app-routing.module';
 
 // components
 import { AppComponent } from './app.component';
@@ -16,6 +20,8 @@ import { AppComponent } from './app.component';
     // angular modules
     BrowserModule,
     HttpClientModule,
+    RouterModule,
+    AppRoutingModule,
     // feature modules
     HomeModule,
     ProductModule
