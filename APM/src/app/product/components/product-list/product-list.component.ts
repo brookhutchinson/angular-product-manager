c8dac431ee1ffb9f6902ba29019c04eb41b94a49
Brook Hutchinson <Brookhutchinson@outlook.com>
2023-07-01 07:35:07 -0500

    Register ProductListComponent inside ProductModule

--- APM/src/app/product/components/product-list/product-list.component.ts
+++ APM/src/app/product/components/product-list/product-list.component.ts
@@ -4,6 +4,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent {}
